eaf3d52e30921b2dcebf5b9380d2c93fb65025f2
ARK2K <ark11.01.2000@gmail.com>
2025-06-19 12:32:00 +0530

    Refactor: Update Header component layout and improve login handling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,48 @@
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const logoutHandler = () => {
     logout();
     navigate('/login');
   };
-  
+
+  const isLoginPage = location.pathname === '/login';
+
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>Recipe Book</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              {user ? (
-                <NavDropdown title={user?.name || 'Account'} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/recipes/new">
-                    <NavDropdown.Item>Add Recipe</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
+      <Navbar bg="dark" variant="dark" expand="md" className="px-3">
+        <Container fluid className="justify-content-between">
+          <Navbar.Brand href="/">Recipe Book</Navbar.Brand>
+          <Nav className="ms-auto">
+            {user ? (
+              <NavDropdown title={user?.name || 'Account'} id="username">
+                <LinkContainer to="/profile">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/recipes/new">
+                  <NavDropdown.Item>Add Recipe</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              !isLoginPage && (
                 <LinkContainer to="/login">
                   <Nav.Link>
                     <i className="fas fa-user"></i> Sign In
                   </Nav.Link>
                 </LinkContainer>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+              )
+            )}
+          </Nav>
         </Container>
       </Navbar>
     </header>
