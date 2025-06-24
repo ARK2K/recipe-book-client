674a8bb2e3b7bbe2904f5a2afa4d9ec6af09abeb
ARK2K <ark11.01.2000@gmail.com>
2025-06-24 10:38:32 +0530

    Fix: Update submitComment method to destructure parameters for improved clarity

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,55 +1,55 @@
-import axiosInstance from '../utils/axiosInstance';
+import axiosInstance from '../utils/axiosInstance';  
 
-const recipeService = {
-  getAllRecipes: async () => {
-    const response = await axiosInstance.get('/api/recipes');
-    return response.data;
-  },
-
-  getRecipeById: async (id) => {
-    const response = await axiosInstance.get(`/api/recipes/${id}`);
-    return response.data;
-  },
-
-  createRecipe: async (recipeData) => {
-    const response = await axiosInstance.post('/api/recipes', recipeData);
-    return response.data;
-  },
-
-  updateRecipe: async (id, recipeData) => {
-    const response = await axiosInstance.put(`/api/recipes/${id}`, recipeData);
-    return response.data;
-  },
-
-  deleteRecipe: async (id) => {
-    const response = await axiosInstance.delete(`/api/recipes/${id}`);
-    return response.data;
-  },
-
-  uploadRecipeImage: async (imageData) => {
-    const response = await axiosInstance.post('/api/recipes/upload', imageData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      withCredentials: true,
-    });
-    return response.data;
-  },
-
-  toggleFavorite: async (id) => {
-    const response = await axiosInstance.post(`/api/recipes/favorites/${id}`);
-    return response.data;
-  },
-
-  submitRating: async (id, stars) => {
-    const response = await axiosInstance.post(`/api/recipes/${id}/rate`, { stars });
-    return response.data;
-  },
-
-  submitComment: async (id, data) => {
-    const response = await axiosInstance.post(`/api/recipes/${id}/comment`, data);
-    return response.data;
-  },
-};
+const recipeService = {  
+  getAllRecipes: async () => {  
+    const response = await axiosInstance.get('/api/recipes');  
+    return response.data;  
+  },  
+  
+  getRecipeById: async (id) => {  
+    const response = await axiosInstance.get(`/api/recipes/${id}`);  
+    return response.data;  
+  },  
+  
+  createRecipe: async (recipeData) => {  
+    const response = await axiosInstance.post('/api/recipes', recipeData);  
+    return response.data;  
+  },  
+  
+  updateRecipe: async (id, recipeData) => {  
+    const response = await axiosInstance.put(`/api/recipes/${id}`, recipeData);  
+    return response.data;  
+  },  
+  
+  deleteRecipe: async (id) => {  
+    const response = await axiosInstance.delete(`/api/recipes/${id}`);  
+    return response.data;  
+  },  
+  
+  uploadRecipeImage: async (imageData) => {  
+    const response = await axiosInstance.post('/api/recipes/upload', imageData, {  
+      headers: {  
+        'Content-Type': 'multipart/form-data',  
+      },  
+      withCredentials: true,  
+    });  
+    return response.data;  
+  },  
+  
+  toggleFavorite: async (id) => {  
+    const response = await axiosInstance.post(`/api/recipes/favorites/${id}`);  
+    return response.data;  
+  },  
+  
+  submitRating: async (id, stars) => {  
+    const response = await axiosInstance.post(`/api/recipes/${id}/rate`, { stars });  
+    return response.data;  
+  },  
+  
+  submitComment: async (id, { comment, rating }) => {  
+    const response = await axiosInstance.post(`/api/recipes/${id}/comment`, { comment, rating });  
+    return response.data;  
+  },  
+};  
 
 export default recipeService;
